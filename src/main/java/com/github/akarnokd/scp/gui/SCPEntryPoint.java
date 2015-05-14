ad262e46b4e9cb956553edae09446604411b6e9e
akarnokd <akarnokd@gmail.com>
2015-05-14 08:25:30 +0200

    Run on server now deploys the Ivy libraries as well.

--- src/main/java/com/github/akarnokd/scp/gui/SCPEntryPoint.java
+++ src/main/java/com/github/akarnokd/scp/gui/SCPEntryPoint.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.widgets.*;
 
 /**
@@ -16,11 +17,28 @@ public class SCPEntryPoint extends AbstractEntryPoint {
         return super.createUI();
     }
 
+    int c;
+    
     @PostConstruct
     @Override
     public void createContents(final Composite parent) {
         Label label = new Label(parent, SWT.NONE);
         label.setText("Hello world!");
+        Button btn = new Button(parent, SWT.PUSH);
+        btn.addSelectionListener(new SelectionAdapter() {
+            /** */
+            private static final long serialVersionUID = 1223221546391601176L;
+
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                label.setText("Clicks: " + (c++));
+            }
+        });
     }
 
 }
